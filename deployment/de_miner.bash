bf35b4c1c3a7ad08aa1dc556abd860a51266ecf3
windy <xfstudio@qq.com>
2017-12-18 06:35:46 +0800

    fix SALT_MASTER

--- deployment/de_miner.bash
+++ deployment/de_miner.bash
@@ -7,11 +7,11 @@ POOL_HOST=192.168.1.143
 POOL_PORT=7777
 MINER_WALLET=46KLU3zxPAtUqKqd5VHQBd3Vxt1quP6JUd2CruioHTog5G34NnubN1Jc7dMtvgZhhq3caBJ2hPacqSWsxPmSJUwZFtd1373
 
-if [ $(sed -n '16p' /etc/salt/minion)x = "master: $HOST_MASTER"x ];
+if [ $(sed -n '16p' /etc/salt/minion)x = "master: $SALT_MASTER"x ];
 then
 echo "$(sed -n '16p' /etc/salt/minion)"
 else
-sed -i "16c master: $HOST_MASTER" /etc/salt/minion
+sed -i "16c master: $SALT_MASTER" /etc/salt/minion
 sed -i "78c id: $(curl ifconfig.me)-$(ifconfig eth0|grep 'inet addr'| awk '{print ($2)}'|awk -F: '{print ($2)}')" /etc/salt/minion
 systemctl enable salt-minion
 systemctl restart salt-minion
