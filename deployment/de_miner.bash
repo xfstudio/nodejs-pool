df6df531305c9b96947128bc30bdb3f947bf58c4
windy <xfstudio@qq.com>
2017-12-18 21:24:09 +0800

    fix shell space

--- deployment/de_miner.bash
+++ deployment/de_miner.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "upgrade miner(rc.local): wget -q https://raw.githubusercontent.com/xfstudio/nodejs-pool/master/deployment/de_miner.bash -O /root/de_miner.bash -N && bash /root/de_miner.bash"
+echo "upgrade miner(rc.local): wget -q https://raw.githubusercontent.com/xfstudio/nodejs-pool/master/deployment/de_miner.bash -O /root/de_miner.bash && bash /root/de_miner.bash"
 echo "Continuing auto run, Please run me as root!"
 
 SALT_MASTER=192.168.1.143
-MIONIN_ID= $(curl ifconfig.me)-$(ifconfig eth0|grep 'inet addr'| awk '{print ($2)}'|awk -F: '{print ($2)}')
+MIONIN_ID=$(curl ifconfig.me)-$(ifconfig eth0|grep 'inet addr'| awk '{print ($2)}'|awk -F: '{print ($2)}')
 POOL_HOST=192.168.1.143
 POOL_PORT=7777
 MINER_WALLET=46KLU3zxPAtUqKqd5VHQBd3Vxt1quP6JUd2CruioHTog5G34NnubN1Jc7dMtvgZhhq3caBJ2hPacqSWsxPmSJUwZFtd1373
